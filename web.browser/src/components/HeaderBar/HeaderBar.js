d5b2b5343779b284318773639003c2e713e2863e
Iris <mdza.iris@gmail.com>
2017-04-12 13:47:26 -0700

    initial commit

---
+++ web.browser/src/components/HeaderBar/HeaderBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import SiteIcon from 'material-ui/svg-icons/communication/import-contacts';
+import FlatButton from 'material-ui/FlatButton';
+
+const HeaderBar = () => (
+  <AppBar
+    title="RED it"
+    iconElementLeft={
+        <SiteIcon />
+    }
+    iconClassName="header-icon"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+    iconElementRight={
+        <div>
+            <FlatButton label="Share a new link" />
+            <FlatButton label="Logout" />
+        </div>
+    }
+  />
+);
+
+export default HeaderBar;
