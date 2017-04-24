11384214db7f4bf63f911bb3bbba427fd65c5f1e
Iris <mdza.iris@gmail.com>
2017-04-24 01:56:30 -0700

    map weeks data in categories component

--- web.browser/src/containers/Categories/Categories.js
+++ web.browser/src/containers/Categories/Categories.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import Drawer from 'material-ui/Drawer';
 import AppBar from 'material-ui/AppBar';
 import SiteIcon from 'material-ui/svg-icons/communication/import-contacts';
-import styles from './styles.css';
+import { List, ListItem } from 'material-ui/List';
+import Week from './../../components/Week';
+import Subheader from 'material-ui/Subheader';
+import Divider from 'material-ui/Divider';
 import { data } from '../../mock-data';
+import styles from './styles.css';
 
 
 class Categories extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { open: true };
+  constructor() {
+    super();
+    this.state = {
+      open: true,
+      weeks: data.weeks,
+    };
   }
-
+  
   render() {
     return (
       <div>
@@ -22,6 +29,14 @@ class Categories extends Component {
               <SiteIcon />
             }
           />
+          <List>
+            {this.state.weeks.map((week) => (
+              <Week
+                key={week.id}
+                week={week}
+              />
+            ))};
+          </List>
         </Drawer>
       </div>
     );
