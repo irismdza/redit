5278285b057e0d94a850b72764a88db26af76151
Iris <mdza.iris@gmail.com>
2017-04-24 01:51:51 -0700

    add mui and props to post component

--- web.browser/src/components/Post/Post.js
+++ web.browser/src/components/Post/Post.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import AppBar from 'material-ui/AppBar';
-import SiteIcon from 'material-ui/svg-icons/communication/import-contacts';
-import FlatButton from 'material-ui/FlatButton';
 
-const HeaderBar = () => (
-  <AppBar
-    title="RED it"
-    iconElementLeft={
-        <SiteIcon />
-    }
-    iconClassNameLeft="header-icon"
-    iconClassNameRight="muidocs-icon-navigation-expand-more"
-    iconElementRight={
-        <div>
-            <FlatButton label="Share a new link" />
-            <FlatButton label="Logout" />
-        </div>
-    }
-  />
+import { Card } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import Chip from 'material-ui/Chip';
+
+const Post = ({ post, updateVote }) => (
+  <Card>
+    {post.link} 
+    <FlatButton 
+    onTouchTap={() => updateVote(post)}
+    >
+      {post.votes ? post.votes : '0' }
+    </FlatButton>
+    <p>{post.title}</p>
+    <p>{post.description}</p>
+    <Chip label="Sort" />
+  </Card>
 );
 
 export default Post;
