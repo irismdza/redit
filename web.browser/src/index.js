0bc931b59b17cd745463a0595d84b58d712be4a3
Iris <mdza.iris@gmail.com>
2017-04-26 09:09:37 -0700

    add imports to root js file

--- web.browser/src/index.js
+++ web.browser/src/index.js
@@ -1,32 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import App from './containers/App';
 import MainLayout from './layouts/MainLayout';
-import HeaderBar from './components/HeaderBar';
-import Categories from './containers/Categories';
-import Week from './components/Week';
+import Welcome from './containers/Welcome';
+import Login from './containers/Login';
+import Post from './components/Post';
+import PostList from './containers/PostList';
+import CreatePost from './containers/CreatePost';
+import store from './redux/store';
 
-
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import muiTheme from './styles/mui-theme';
 import './styles/index.css';
 
 // Needed for onTouchTap (Material UI)
 // http://stackoverflow.com/a/34015469/988941
-import injectTapEventPlugin from 'react-tap-event-plugin';
+
 injectTapEventPlugin();
 
 ReactDOM.render(
   <MuiThemeProvider muiTheme={muiTheme}>
-    <MainLayout>
-      <App>
-        <HeaderBar />
-        <Categories>
-          <Week />
-        </Categories>
-      </App>
-    </MainLayout>
+    <Provider store={store}>
+      <Router>
+        <MainLayout>
+          <App>
+            <Switch>
+              <Route exact path="/" component={Welcome} />
+              <Route path="Login" component={Login} />
+              <Route exact path="/createpost" component={CreatePost} />
+              <Route exact path="/postlist" component={PostList} />
+              <Route exact path="/post" component={Post} />
+            </Switch>
+          </App>
+        </MainLayout>
+      </Router>
+    </Provider>
   </MuiThemeProvider>,
   document.getElementById('root')
 );
