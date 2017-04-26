f3880afa9513fc2f989e7f997ae16e6f80f57383
Iris <mdza.iris@gmail.com>
2017-04-26 16:52:10 -0700

    Updated postReducer

--- web.browser/src/redux/postReducer.js
+++ web.browser/src/redux/postReducer.js
@@ -1,22 +1,21 @@
 import { data } from './../mock-data';
 import { UPDATE_VOTE } from './actions';
 
+
 const initialState = data.posts;
 
-const postReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case UPDATE_VOTE:
-            const newPostList = state.posts.map((post) => {
-                if (action.payload.id === post.id) {
-                    post.votes++;
-                }
+export function postReducer(state = initialState, action) {
+  switch (action.type) {
+    case UPDATE_VOTE:
+        const newPostList = state.posts.map((post) => {
+          if (action.payload.id === post.id) {
+            state.posts.votes++;
+          }
                 console.log('vote');
                 return post;
             });
             return newPostList;
-        default:
-            return state;
-    }
-};
-
-export default postReducer;
+    default:
+      return state;
+  }
+}
