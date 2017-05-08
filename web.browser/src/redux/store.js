e152e183a9e0b24e294b357a52b15b777b46a459
Iris <mdza.iris@gmail.com>
2017-05-08 09:56:43 -0700

    add reducers to store

--- web.browser/src/redux/store.js
+++ web.browser/src/redux/store.js
@@ -1,16 +1,19 @@
+import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import reduxLogger from 'redux-logger';
 import { PostReducer } from './postReducer';
-import { SortNewestReducer } from './sortNewestReducer';
-import { SortMostPopularReducer } from './sortMostPopularReducer';
+import { LessonReducer } from './../redux/selectedLessonReducer';
+import { WeeksReducer } from './weeksReducer';
 
 export default createStore(
-    combineReducers({
-      posts: PostReducer, SortNewestReducer, SortMostPopularReducer
-      // weeks: WeeksReducer,
-    }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(
-        reduxLogger
-        ),
+  combineReducers({
+    posts: PostReducer,
+    weeks: WeeksReducer,
+    selectedLesson: LessonReducer,
+  }),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(
+    reduxLogger,
+    thunk
+  ),
 );
