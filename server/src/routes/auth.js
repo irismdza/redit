7b29266cc5ef118dd5229a699d49783cad0ad6c2
Iris <mdza.iris@gmail.com>
2017-05-08 10:05:59 -0700

    working commit for server side

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,16 +1,35 @@
-const pool = require('../index');
+const pool = require('./index');
+const jwt = require('jsonwebtoken');
 
 module.exports = function(router) {
 
   router.post('/login',(req, res) => {
+    console.log(req.body);
+
     const{ email, password } = req.body;
 
-    pool.query(`SELECT * FROM users WHERE email='${email}' AND password=crypt('${password}, password);`)
+    pool.query(`SELECT * FROM users WHERE email='${email}' AND password=crypt('${password}', password);`)
       .then((users) => {
         if(users && users.rows.length) {
-          return res.status(200).send(user.rows);
-        } else return res.status(403).send('Invalid username or password...');
-      });
+          // MAKE A STATELESS SESSION:
+          const session = {
+            user_email: users.rows[0].email
+          }
+          const JWT = jwt.sign(session, 'jwealkfja');
+          console.log('this is the jwt', JWT);
+
+          res.status(200).cookie('redit_session', JWT, {
+            secure: false,
+            maxAge: 7200000,
+            httpOnly: true
+          }).send('Success, you\'re logged in!');
+          console.log('this is the session', session);
+        } else {
+          res.status(403).send();
+        }
+      }).catch((err) => {
+        res.status(500).send();
+  })
   });
 
   router.get('/logout', (req, res) => {
